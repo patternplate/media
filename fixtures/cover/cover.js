0752af9a1da2a13f6a6e8b4ed9e7a4f893094aaf
Mario Nebl <hello@mario-nebl.de>
2018-03-18 13:06:57 +0100

    add hello world cover shot

---
+++ fixtures/cover/cover.js
@@ -0,0 +1,6 @@
+module.exports = {
+  default: () => {},
+  html: () => {
+    return '<h1>Hello world</h1>'
+  }
+};
---
+++ fixtures/cover/screenshot.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  url: {
+    query: {
+      "components-enabled": true,
+      "screenshot": true,
+      "reload": true
+    }
+  },
+  svg: {
+    outWidth: 1000,
+    outHeight: 380,
+    viewBox: "0 0 500 300",
+    preserveAspectRatio: "xMinYMin slice"
+  }
+};
