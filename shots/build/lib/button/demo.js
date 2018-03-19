45ac4b0938e759eac3e91b81426801570a40a4e5
Mario Nebl <hello@mario-nebl.de>
2018-03-19 13:02:09 +0100

    Add build result shot

---
+++ shots/build/lib/button/demo.js
@@ -0,0 +1,15 @@
+   module.exports = {
+     html: () => '<button class="my-button">ES2015 Button</button>',
+     css: () => `
+      .my-button {
+        padding: 10px 15px;
+        font-size: 20px;
+        background: none;
+        color: cornflowerblue;
+        border: 1px solid currentColor;
+      }',
+     `,
+     default: () => {
+       // Nothing implemented yet
+     }
+   }
