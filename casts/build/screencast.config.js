0132c519e46788f731d218836504f2d051e83dbc
chrisanderss0n <christopher@nycode.co>
2018-04-12 11:12:08 -0400

    Updated screencast to be aligned with documentation, fixed hardcoded path to use process.cwd instead.

--- casts/build/screencast.config.js
+++ casts/build/screencast.config.js
@@ -1,15 +1,18 @@
 module.exports = {
   cast: `
     printf '\\033[0;32m%s\\033[0m' "λ "
-    echo "yarn patternplate build --out=docs/patterns --base=/patterns/" | pv -qL $[10+(-2 + RANDOM%5)]
-    ../../node_modules/.bin/patternplate build --base="/" --out="docs"
+    echo "yarn patternplate build --out='docs/patterns' --base='/patterns/'" | pv -qL $[10+(-2 + RANDOM%5)]
+    cd ..
+    cd ..
+    ./node_modules/.bin/patternplate build --out='docs/patterns' --base='/patterns/'
 
     printf '\\033[0;32m%s\\033[0m' "λ "
+    sleep 10
     echo ""
   `,
   transform(svg) {
-    // TODO: do not hardcode this directory
-    return svg.split('/Users/marneb/Projects/pp/media/casts/build/docs').join('docs/');
+    let workingDir = process.cwd() + '/docs/patterns';
+    return svg.split(workingDir).join('docs/patterns/');
   },
   term: {
     window: true,
