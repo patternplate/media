205a6b4c834d78a12ccba79913b224c7c6829d19
Christopher Andersson <christopher@nycode.co>
2018-04-12 11:40:09 -0400

    Resync sequence with docs
    * fix: updated text in terminal cast to match documentation
    
    * Updated screencast to be aligned with documentation, fixed hardcoded path to use process.cwd instead.
    
    * style: simplify split a bit
    
    * chore: remove package-lock.json

--- casts/build/screencast.config.js
+++ casts/build/screencast.config.js
@@ -1,14 +1,17 @@
 module.exports = {
   cast: `
     printf '\\033[0;32m%s\\033[0m' "λ "
-    echo "patternplate build --base='/' --out='docs'" | pv -qL $[10+(-2 + RANDOM%5)]
-    ../../node_modules/.bin/patternplate build --base="/" --out="docs"
+    echo "yarn patternplate build --out='docs/patterns' --base='/patterns/'" | pv -qL $[10+(-2 + RANDOM%5)]
+    cd ..
+    cd ..
+    ./node_modules/.bin/patternplate build --out='docs/patterns' --base='/patterns/'
 
     printf '\\033[0;32m%s\\033[0m' "λ "
+    sleep 10
     echo ""
   `,
   transform(svg) {
-    return svg.split('/Users/marneb/Projects/pp/media/casts/build/docs').join('docs/');
+    return svg.split(process.cwd() + '/docs/patterns').join('docs/patterns/');
   },
   term: {
     window: true,
